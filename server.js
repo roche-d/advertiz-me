a7a2030633909027fada1749db0735707d855789
roche_d <roche_d@epitech.eu>
2015-03-15 00:41:08 +0100

    Game Process

--- server.js
+++ server.js
@@ -12,7 +12,23 @@ GameServiceProcess = require("./game.js");
 GameServiceProcess.startGame(game);
 
 
+
+var fs = require('fs');
+var https = require('https');
+var privateKey  = fs.readFileSync('privateKey.key', 'utf8');
+var certificate = fs.readFileSync('certificate.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
+// your express configuration here
+
+var httpsServer = https.createServer(credentials, app);
+
+
+
+
 app.use(['/game/*'], game);
 app.use(['/'], express.static(__dirname + '/web/www'));
 
+httpsServer.listen(443);
 server.listen(8080);
